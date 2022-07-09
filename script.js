26875b8d42abf5d0aa1c60d7d005458a5f312b81
bprecia1 <107278642+bprecia1@users.noreply.github.com>
2022-07-09 16:50:34 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,81 @@
+const quotes = [
+    '"‘Does it hurt?’ The childish question had escaped Harrys lips before he could stop it. ‘Dying? Not at all,’ said Sirius. ‘Quicker and easier than falling asleep.',
+    'That wand’s more trouble than it’s worth. And quite honestly, I’ve had enough trouble for a lifetime.',
+    'We’re all human, aren’t we? Every human life is worth the same, and worth saving.',
+    'I’ve always wanted to use that spell.',
+    'Do not pity the dead, Harry. Pity the living, and, above all, those who live without love.',
+
+];
+// store the list of words and the index of the word the player is currently typing
+let words = [];
+let wordIndex = 0;
+// the starting time
+let startTime = Date.now();
+// page elements
+const quoteElement = document.getElementById('quote');
+const messageElement = document.getElementById('message');
+const typedValueElement = document.getElementById('typed-value');
+
+document.getElementById('start').addEventListener('click', () => {
+    // get a quote
+    const quoteIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[quoteIndex];
+    // Put the quote into an array of words
+    words = quote.split(' ');
+    // reset the word index for tracking
+    wordIndex = 0;
+   
+    // UI updates
+    // Create an array of span elements so we can set a class
+    const spanWords = words.map(function(word) { return `<span>${word} </span>`});
+    // Convert into string and set as innerHTML on quote display
+    quoteElement.innerHTML = spanWords.join('');
+    // Highlight the first word
+    quoteElement.childNodes[0].className = 'highlight';
+    // Clear any prior messages
+    messageElement.innerText = '';
+   
+    // Setup the textbox
+    // Clear the textbox
+    typedValueElement.value = '';
+    // set focus
+    typedValueElement.focus();
+    // set the event handler
+   
+    // Start the timer
+    startTime = new Date().getTime();
+  });
+
+  typedValueElement.addEventListener('input', () => {
+    // Get the current word
+    const currentWord = words[wordIndex];
+    // get the current value
+    const typedValue = typedValueElement.value;
+   
+    if (typedValue === currentWord && wordIndex === words.length - 1) {
+      // end of sentence
+      // Display success
+      const elapsedTime = new Date().getTime() - startTime;
+      const message = `CONGRATULATIONS! You finished in ${elapsedTime / 1000} seconds.`;
+      messageElement.innerText = message;
+    } else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
+      // end of word
+      // clear the typedValueElement for the new word
+      typedValueElement.value = '';
+      // move to the next word
+      wordIndex++;
+      // reset the class name for all elements in quote
+      for (const wordElement of quoteElement.childNodes) {
+        wordElement.className = '';
+      }
+      // highlight the new word
+      quoteElement.childNodes[wordIndex].className = 'highlight';
+    } else if (currentWord.startsWith(typedValue)) {
+      // currently correct
+      // highlight the next word
+      typedValueElement.className = '';
+    } else {
+      // error state
+      typedValueElement.className = 'error';
+    }
+  });
